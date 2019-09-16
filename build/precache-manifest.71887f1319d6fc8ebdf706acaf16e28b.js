439648b7ca0b0fecae5ca138a6bb1a580023dddb
Kim Jae Hyune <wogus4041@naver.com>
2019-09-16 16:32:12 +0900

    빌드

---
+++ build/precache-manifest.71887f1319d6fc8ebdf706acaf16e28b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b80e408b8f78beb1cafde96c6f0ac43",
+    "url": "/ui_ux_portfolio/index.html"
+  },
+  {
+    "revision": "90f25fa3339d453f3539",
+    "url": "/ui_ux_portfolio/static/js/2.d9f23ba9.chunk.js"
+  },
+  {
+    "revision": "d4454edf70cebfa11d69",
+    "url": "/ui_ux_portfolio/static/js/main.936c77f9.chunk.js"
+  },
+  {
+    "revision": "05739a5c925b37e9e597",
+    "url": "/ui_ux_portfolio/static/js/runtime~main.68098432.js"
+  }
+]);
