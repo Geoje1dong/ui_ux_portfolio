8af4921194b7506dc46a912d7d584dcd55f8d759
Kim Jae Hyune <wogus4041@naver.com>
2019-09-16 16:24:08 +0900

    이력서 내용 수정

---
+++ build/precache-manifest.021e5ac70959ea22acf50c658c9ca6f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e7868c7ae85c2016ce29a6c5e0f128",
+    "url": "/ui_ux_portfolio/index.html"
+  },
+  {
+    "revision": "90f25fa3339d453f3539",
+    "url": "/ui_ux_portfolio/static/js/2.d9f23ba9.chunk.js"
+  },
+  {
+    "revision": "1e8925648fbb51153ade",
+    "url": "/ui_ux_portfolio/static/js/main.74902731.chunk.js"
+  },
+  {
+    "revision": "05739a5c925b37e9e597",
+    "url": "/ui_ux_portfolio/static/js/runtime~main.68098432.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.86e5d7663baa35ac5591a3f1da0c8ea3.js"
+  "/ui_ux_portfolio/precache-manifest.021e5ac70959ea22acf50c658c9ca6f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ui_ux_portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
