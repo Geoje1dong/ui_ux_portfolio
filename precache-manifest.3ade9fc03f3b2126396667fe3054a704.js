2f7c22f91cc381c15031b7b45268d66e2e0c6b64
Kim Jae Hyune <wogus4041@naver.com>
2019-10-07 23:12:31 +0900

    Updates

---
+++ precache-manifest.3ade9fc03f3b2126396667fe3054a704.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d42537b2bf0382b848bafa5676f24f",
+    "url": "/ui_ux_portfolio/index.html"
+  },
+  {
+    "revision": "272e488f925dd48071a9",
+    "url": "/ui_ux_portfolio/static/js/2.789a34bd.chunk.js"
+  },
+  {
+    "revision": "9fdc2b7d96ca76af724a",
+    "url": "/ui_ux_portfolio/static/js/main.42b74401.chunk.js"
+  },
+  {
+    "revision": "05739a5c925b37e9e597",
+    "url": "/ui_ux_portfolio/static/js/runtime~main.68098432.js"
+  }
+]);
